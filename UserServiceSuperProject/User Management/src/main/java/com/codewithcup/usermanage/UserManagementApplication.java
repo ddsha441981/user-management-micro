72455acda857490e30397d93ab8608d02a458bd7
ddsha441981 <ddsa44191@gmail.com>
2021-08-24 17:28:46 +0530

    project setup completed....

---
+++ UserServiceSuperProject/User Management/src/main/java/com/codewithcup/usermanage/UserManagementApplication.java
@@ -0,0 +1,14 @@
+package com.codewithcup.usermanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello *******************************");
+        SpringApplication.run(UserManagementApplication.class, args);
+    }
+
+}
